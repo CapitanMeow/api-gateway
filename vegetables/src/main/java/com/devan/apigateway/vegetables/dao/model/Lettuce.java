158c2c8d24a52c2b951d4365a50805d61f1dcdc0
CapitanMeow <vangelis.manganas@gmail.com>
2018-08-26 17:06:14 +0300

    minor fixes

--- vegetables/src/main/java/com/devan/apigateway/vegetables/dao/model/Lettuce.java
+++ vegetables/src/main/java/com/devan/apigateway/vegetables/dao/model/Lettuce.java
@@ -4,8 +4,11 @@ import com.devan.apigateway.vegetables.dao.enums.LettuceType;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+
 @Getter
 @Setter
+@Entity(name = "LETTUCE")
 public class Lettuce extends Vegetable {
 
     private LettuceType type;
--- vegetables/src/main/java/com/devan/apigateway/vegetables/dao/model/Tomato.java
+++ vegetables/src/main/java/com/devan/apigateway/vegetables/dao/model/Tomato.java
@@ -4,8 +4,11 @@ import com.devan.apigateway.vegetables.dao.enums.TomatoType;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+
 @Getter
 @Setter
+@Entity(name = "TOMATO")
 public class Tomato extends Vegetable {
 
     private TomatoType type;
